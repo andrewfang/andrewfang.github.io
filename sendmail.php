76f66e6cfade92f03d91b55c4e5cb7d744ab15e4
andrewfang <andrewbrianfang@gmail.com>
2013-07-05 21:19:48 -0700

    Fixed email for about page

--- sendmail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "andrewfang@outlook.com";
-$subject = "Test mail";
-$message = "Hello! This is a simple email message.";
-$from = "andrewfang@outlook.com";
-$headers = "From:" . $from;
-mail($to,$subject,$message,$headers);
-echo "Mail Sent.";
-?>
