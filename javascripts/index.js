195add887077f9adc5b3420f47b159ed11339405
andrewfang <andrewbrianfang@gmail.com>
2013-08-27 11:16:42 -0700

    Adaptations for mobile browsers.  Updated Info page

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,16 +5,20 @@ function initialize(event){
 
 function colorize(event){
 	$(this).closest("div").find("h2").css("color", event.data.color);
+	$("#infoOnLinks").text(event.data.link);
+	$("#infoOnLinks").animate({
+		marginTop: "5px",
+		opacity: 1
+	});
+}
+function decolorize(event){
+	$(this).closest("div").find("h2").css("color", event.data.color);
 }
 function colorizephoto(event){
 	var randomcolor = '#'+Math.floor(Math.random()*16777215).toString(16);
 	$(this).closest(".horizblocks").find("#profile").css("background", randomcolor);
 }
 
-// function show(event){
-// 	$(".horizblocks").hide();
-// 	$(event.data.element).slideDown();
-// }
 function show(event) {
 	if ($(this).hasClass("disabled")) {
 		return false;
@@ -22,20 +26,24 @@ function show(event) {
 		event.preventDefault();
 		var currenthash = $(this).attr("href");
 		$(".horizblocks").slideUp(300).delay(600);
-		$(currenthash).slideDown(800).delay(400);
 		window.location = currenthash;
 		$(".button").removeClass("disabled");
 		if ($(this).hasClass("button")) {
 			$(this).addClass("disabled");
 		}
+		if (currenthash != "#home") {
+			$(currenthash).slideDown(800).delay(400);			
+		}
 	}
+	$("#infoOnLinks").animate({opacity: 0});
 }
 
 $(document).ready(function(){
 	initialize();
 
+	// Hashing for url
 	var arr = ["#home", "#projects", "#about"];
-	if (window.location.hash == "") {
+	if (window.location.hash == "" || window.location.hash == "#home") {
 		$("#homebutton").addClass("disabled");
 	} else if ($.inArray(window.location.hash, arr) !== -1) {
 		$(window.location.hash).slideDown(300);
@@ -45,22 +53,24 @@ $(document).ready(function(){
 	}
 
 	// Color the picture border on mouseover/leave of different elements
-	$("#linkedin").on("mouseover", {color:"#0073b2"}, colorize);
-	$("#facebook").on("mouseover", {color:"#465fa1"}, colorize);
-	$("#gplus").on("mouseover", {color:"#b43021"}, colorize);
-	$("#twitter").on("mouseover", {color:"#00acee"}, colorize);
-	$("#hkn").on("mouseover", {color:"#f7e26b"}, colorize);
-	$("#github").on("mouseover", {color:"#657e8c"}, colorize);
-	$("#schedule").on("mouseover", {color:"#fdb000"}, colorize);
-	$("#email").on("mouseover", {color:"#cf2a1c"}, colorize);
-	$(".aboutmelink").on("mouseleave", {color:"#000000"}, colorize);
+	$("#linkedin").on("mouseover", {color:"#0073b2", link:"LinkedIn"}, colorize);
+	$("#facebook").on("mouseover", {color:"#465fa1", link:"Facebook"}, colorize);
+	$("#gplus").on("mouseover", {color:"#b43021", link:"Google Plus"}, colorize);
+	$("#twitter").on("mouseover", {color:"#00acee", link:"Twitter"}, colorize);
+	$("#hkn").on("mouseover", {color:"#f7e26b", link:"HKN WebPage"}, colorize);
+	$("#github").on("mouseover", {color:"#657e8c", link:"GitHub"}, colorize);
+	$("#schedule").on("mouseover", {color:"#fdb000", link:"Class Schedule"}, colorize);
+	$("#email").on("mouseover", {color:"#cf2a1c", link:"Email me"}, colorize);
+	$(".aboutmelink").on("mouseleave", {color:"#000000"}, decolorize);
+
 
 	// Color the main profile picture
 	$("#homebutton").on("mouseover", colorizephoto);
 	$("#projectsbutton").on("mouseover", colorizephoto);
 	$("#aboutbutton").on("mouseover", colorizephoto);
 
-	// $("#homebutton").on("click", {element:"#"} show);
+	// For navigating about the website
+	$("#homebutton").on("click", show);
 	$("#projectsbutton").on("click", show);
 	$("#aboutbutton").on("click", show);
 
@@ -69,7 +79,6 @@ $(document).ready(function(){
 	$(".projectContainer").on("mouseover", function(){
 		$(this).find(".projectBackground").css("opacity", .3);
 	})
-
 	$(".projectContainer").on("mouseleave", function(){
 		$(this).find(".projectBackground").css("opacity", 0);
 	})
